ea9d8b4d9f7e03e6288e442d07466eaab6ed5a0a
LeeJaeHoon <dlwogns3413@naver.com>
2021-11-28 20:01:53 +0900

    clear section3 2

--- section3/2.js
+++ section3/2.js
@@ -1,7 +1,24 @@
 function solution(s) {
-  let answer;
+  let answer = "YES";
+  const n = s.length / 2;
+  const str = s.toLowerCase();
+  for (let i = 0; i < Math.floor(n); i++) {
+    if (
+      96 < s[i].charCodeAt &&
+      123 > s[i].charCodeAt &&
+      s[i] !== str[n - i - 1]
+    )
+      answer = "NO";
+  }
+  return answer;
+}
+
+function solution2(s) {
+  let answer = "YES";
+  s = s.toLowerCase().replace(/[^a-z]/g, "");
+  if (s !== s.split("").reverse().join("")) answer = "NO";
   return answer;
 }
 
 let str = "found7, time: study; Yduts; emit, 7Dnuof";
-console.log(solution(str));
+console.log(solution2(str));
