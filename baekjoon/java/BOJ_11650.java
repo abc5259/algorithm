a9175d284f971b971a4038f504a122a336495bd3
LeeJaeHoon <dlwogns3413@naver.com>
2022-03-25 21:49:14 +0900

    BOJ: 정렬 문제 1개 working

---
+++ baekjoon/java/BOJ_11650.java
@@ -0,0 +1,16 @@
+package baekjoon.java;
+
+import java.util.Scanner;
+
+public class BOJ_11650 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    StringBuffer sb = new StringBuffer();
+    int N = sc.nextInt();
+    for(int i=0; i<N; i++) {
+      int x = sc.nextInt();
+      int y = sc.nextInt();
+      
+    }
+  }
+}
