73bf1092c20492cfbce713f72b19c55e96b09237
LeeJaeHoon <dlwogns3413@naver.com>
2022-04-15 15:43:42 +0900

    BOJ: 1문제 clear

--- baekjoon/java/BOJ_2089.java
+++ baekjoon/java/BOJ_2089.java
@@ -1,11 +1,19 @@
 package baekjoon.java;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class BOJ_2089 {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    int N = -13;
-    System.out.println(-13%-2);
+    List<Integer> list = new ArrayList<>();
+    int n = sc.nextInt();
+    if(n == 0) 
+      System.out.println(n);
+    while(n != 0) {
+      list.add(Math.abs(n%-2));
+      n = (int)Math.ceil((double)n / -2);
+    }
+    for(int i=list.size()-1; i>=0; i--)
+      System.out.print(list.get(i));
   }
 }
